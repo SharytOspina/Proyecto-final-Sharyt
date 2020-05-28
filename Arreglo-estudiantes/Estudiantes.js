1ad103cb72f3cde632fe165b6e89102ecd64a39b
SharytOspina <61770005+SharytOspina@users.noreply.github.com>
2020-05-28 00:39:47 -0500

    Add files via upload

---
+++ Arreglo-estudiantes/Estudiantes.js
@@ -0,0 +1,506 @@
+var estudiante1=[]; //Creo el arreglo estudiantes
+var estudiante1=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=37441; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante1.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante1.push(" "+ "Apellido: "+ apellido );
+estudiante1.push(" "+ "Codigo: "+ codigo );
+estudiante1.push(" "+ "Dias vividos: "+ dvividos );
+estudiante1.push(" "+ "Edad: "+ edad );
+estudiante1.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 1: " +estudiante1 + "<br>");
+
+
+
+// Estudiante 2
+
+var estudiante2=[]; //Creo el arreglo estudiantes
+var estudiante2=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=40385; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante2.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante2.push(" "+ "Apellido: "+ apellido );
+estudiante2.push(" "+ "Codigo: "+ codigo );
+estudiante2.push(" "+ "Dias vividos: "+ dvividos );
+estudiante2.push(" "+ "Edad: "+ edad );
+estudiante2.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 2: " +estudiante2 + "<br>");
+
+
+// Estudiante 3
+
+var estudiante3=[]; //Creo el arreglo estudiantes
+var estudiante3=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=39828; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante3.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante3.push(" "+ "Apellido: "+ apellido );
+estudiante3.push(" "+ "Codigo: "+ codigo );
+estudiante3.push(" "+ "Dias vividos: "+ dvividos );
+estudiante3.push(" "+ "Edad: "+ edad );
+estudiante3.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 3: " +estudiante3 + "<br>");
+
+
+// Estudiante 4
+
+var estudiante4=[]; //Creo el arreglo estudiantes
+var estudiante4=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=38725; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante4.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante4.push(" "+ "Apellido: "+ apellido );
+estudiante4.push(" "+ "Codigo: "+ codigo );
+estudiante4.push(" "+ "Dias vividos: "+ dvividos );
+estudiante4.push(" "+ "Edad: "+ edad );
+estudiante4.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 4: " +estudiante4 + "<br>");
+
+
+// Estudiante 5
+
+var estudiante5=[]; //Creo el arreglo estudiantes
+var estudiante5=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=38239; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante5.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante5.push(" "+ "Apellido: "+ apellido );
+estudiante5.push(" "+ "Codigo: "+ codigo );
+estudiante5.push(" "+ "Dias vividos: "+ dvividos );
+estudiante5.push(" "+ "Edad: "+ edad );
+estudiante5.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 5: " +estudiante5 + "<br>");
+
+
+// Estudiante 6
+
+var estudiante6=[]; //Creo el arreglo estudiantes
+var estudiante6=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=37558; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante6.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante6.push(" "+ "Apellido: "+ apellido );
+estudiante6.push(" "+ "Codigo: "+ codigo );
+estudiante6.push(" "+ "Dias vividos: "+ dvividos );
+estudiante6.push(" "+ "Edad: "+ edad );
+estudiante6.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 6: " +estudiante6 + "<br>");
+
+
+// Estudiante 7
+
+var estudiante7=[]; //Creo el arreglo estudiantes
+var estudiante7=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=37788; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante7.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante7.push(" "+ "Apellido: "+ apellido );
+estudiante7.push(" "+ "Codigo: "+ codigo );
+estudiante7.push(" "+ "Dias vividos: "+ dvividos );
+estudiante7.push(" "+ "Edad: "+ edad );
+estudiante7.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 7: " +estudiante7 + "<br>");
+
+
+// Estudiante 8
+
+var estudiante8=[]; //Creo el arreglo estudiantes
+var estudiante8=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=39440; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante8.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante8.push(" "+ "Apellido: "+ apellido );
+estudiante8.push(" "+ "Codigo: "+ codigo );
+estudiante8.push(" "+ "Dias vividos: "+ dvividos );
+estudiante8.push(" "+ "Edad: "+ edad );
+estudiante8.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 8: " +estudiante8 + "<br>");
+
+
+// Estudiante 9
+
+var estudiante9=[]; //Creo el arreglo estudiantes
+var estudiante9=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=38067; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante9.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante9.push(" "+ "Apellido: "+ apellido );
+estudiante9.push(" "+ "Codigo: "+ codigo );
+estudiante9.push(" "+ "Dias vividos: "+ dvividos );
+estudiante9.push(" "+ "Edad: "+ edad );
+estudiante9.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 9: " +estudiante9 + "<br>");
+
+
+// Estudiante 10
+
+var estudiante10=[]; //Creo el arreglo estudiantes
+var estudiante10=new Array();
+
+//Pedir el codigo y se guarda en la variable codigo
+var codigo= prompt("Codigo: ");
+
+//Pedir el nombre y se guarda en la variable nombre
+var nombre= prompt("Nombre: ");
+
+//Pedir el apellido y se guarda en la variable apellido
+var apellido= prompt("Apellido");
+
+//Pedir el curso y se guarda en la variable curso
+var curso= prompt("Curso: ");
+
+
+var hoy=43993; //Declaro la variable hoy para guardar la fecha actual 
+var nacimiento=38397; //Declaro la variable nacimiento para guardar la fecha de nacimiento 
+var dvividos=hoy-nacimiento; //Calcula los dias vividos
+
+var añoactual= 2020;//Declaro y defino el año actual
+var añonacimiento= prompt("Año de nacimiento: ");//Pedir por teclado el año de nacimiento
+var edad=añoactual-añonacimiento;//Calcula la edad del estudiante
+
+//Pedir las 5 notas
+var n1=prompt("Nota 1: ");
+var n2=prompt("Nota 2: ");
+var n3=prompt("Nota 3: ");
+var n4=prompt("Nota 4: ");
+var n5=prompt("Nota 5: ");
+
+//Promedio de las notas
+//Convierto los valores ingresados de notas de una cadena de texto a un numero decimal usando parseFloat() 
+var promedio=(parseFloat(n1)+parseFloat(n2)+parseFloat(n3)+parseFloat(n4)+parseFloat(n5))/5;
+
+//Agregar los datos al arreglo
+estudiante10.push("<br>"+ " "+ "Nombre: "+ nombre );
+estudiante10.push(" "+ "Apellido: "+ apellido );
+estudiante10.push(" "+ "Codigo: "+ codigo );
+estudiante10.push(" "+ "Dias vividos: "+ dvividos );
+estudiante10.push(" "+ "Edad: "+ edad );
+estudiante10.push(" "+ "Promedio de notas: "+ promedio + "<br>");
+
+//nombre, apellido , codigo , curso , dvividos ,edad , promedio);
+
+document.write("Estudiante 10: " +estudiante10 + "<br>");
